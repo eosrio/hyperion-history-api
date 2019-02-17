407bb1d2746402224cf068a39c056e6467636b06
Igor Lins e Silva <igor@eosrio.io>
2019-02-17 10:57:51 -0300

    Update mappings.js

--- mappings.js
+++ mappings.js
@@ -10,111 +10,113 @@ const action = {
         "index.codec": "best_compression"
     },
     "mappings": {
-        "properties": {
-            "@data": {
-                "properties": {
-                    "transfer": {
-                        "properties": {
-                            "from": {
-                                "type": "keyword"
-                            },
-                            "to": {
-                                "type": "keyword"
-                            },
-                            "amount": {
-                                "type": "float"
-                            },
-                            "symbol": {
-                                "type": "keyword"
-                            },
-                        }
-                    },
-                    "eosio-newaccount.newact": {
-                        "type": "keyword"
-                    },
-                    "forum-vote": {
-                        "properties": {
-                            "proposal": {
-                                "type": "keyword"
-                            },
-                            "vote": {
-                                "type": "byte"
+        "_doc": {
+            "properties": {
+                "@data": {
+                    "properties": {
+                        "transfer": {
+                            "properties": {
+                                "from": {
+                                    "type": "keyword"
+                                },
+                                "to": {
+                                    "type": "keyword"
+                                },
+                                "amount": {
+                                    "type": "float"
+                                },
+                                "symbol": {
+                                    "type": "keyword"
+                                },
+                            }
+                        },
+                        "eosio-newaccount.newact": {
+                            "type": "keyword"
+                        },
+                        "forum-vote": {
+                            "properties": {
+                                "proposal": {
+                                    "type": "keyword"
+                                },
+                                "vote": {
+                                    "type": "byte"
+                                }
                             }
                         }
                     }
+                },
+                "timestamp": {
+                    "type": "date"
+                },
+                "depth": {
+                    "type": "byte"
+                },
+                "trx_id": {
+                    "type": "keyword"
+                },
+                "producer": {
+                    "type": "keyword"
+                },
+                "parent.root": {
+                    "type": "boolean"
+                },
+                "parent.seq": {
+                    "type": "long"
+                },
+                "context_free": {
+                    "enabled": false
+                },
+                "receipt.receiver": {
+                    "type": "keyword"
+                },
+                "receipt.global_sequence": {
+                    "type": "long"
+                },
+                "receipt.abi_sequence": {
+                    "enabled": false
+                },
+                "receipt.act_digest": {
+                    "enabled": false
+                },
+                "receipt.recv_sequence": {
+                    "enabled": false
+                },
+                "receipt.auth_sequence": {
+                    "enabled": false
+                },
+                "receipt.code_sequence": {
+                    "enabled": false
+                },
+                "block_num": {
+                    "type": "long"
+                },
+                "act.account": {
+                    "type": "keyword"
+                },
+                "act.name": {
+                    "type": "keyword"
+                },
+                "act.data": {
+                    "enabled": false
+                },
+                "act.authorization.actor": {
+                    "type": "keyword"
+                },
+                "act.authorization.permission": {
+                    "enabled": false
+                },
+                "elapsed": {
+                    "type": "long"
+                },
+                "except": {
+                    "enabled": false
+                },
+                "account_ram_deltas.account": {
+                    "enabled": false
+                },
+                "account_ram_deltas.delta": {
+                    "enabled": false
                 }
-            },
-            "timestamp": {
-                "type": "date"
-            },
-            "depth": {
-                "type": "byte"
-            },
-            "trx_id": {
-                "type": "keyword"
-            },
-            "producer": {
-                "type": "keyword"
-            },
-            "parent.root": {
-                "type": "boolean"
-            },
-            "parent.seq": {
-                "type": "long"
-            },
-            "context_free": {
-                "enabled": false
-            },
-            "receipt.receiver": {
-                "type": "keyword"
-            },
-            "receipt.global_sequence": {
-                "type": "long"
-            },
-            "receipt.abi_sequence": {
-                "enabled": false
-            },
-            "receipt.act_digest": {
-                "enabled": false
-            },
-            "receipt.recv_sequence": {
-                "enabled": false
-            },
-            "receipt.auth_sequence": {
-                "enabled": false
-            },
-            "receipt.code_sequence": {
-                "enabled": false
-            },
-            "block_num": {
-                "type": "long"
-            },
-            "act.account": {
-                "type": "keyword"
-            },
-            "act.name": {
-                "type": "keyword"
-            },
-            "act.data": {
-                "enabled": false
-            },
-            "act.authorization.actor": {
-                "type": "keyword"
-            },
-            "act.authorization.permission": {
-                "enabled": false
-            },
-            "elapsed": {
-                "type": "long"
-            },
-            "except": {
-                "enabled": false
-            },
-            "account_ram_deltas.account": {
-                "enabled": false
-            },
-            "account_ram_deltas.delta": {
-                "enabled": false
             }
         }
     }
@@ -132,12 +134,14 @@ const transaction = {
         "index.codec": "best_compression"
     },
     "mappings": {
-        "properties": {
-            "block_num": {
-                "type": "long"
-            },
-            "timestamp": {
-                "type": "date"
+        "_doc": {
+            "properties": {
+                "block_num": {
+                    "type": "long"
+                },
+                "timestamp": {
+                    "type": "date"
+                }
             }
         }
     }
@@ -153,26 +157,28 @@ const account = {
         "index.codec": "best_compression"
     },
     "mappings": {
-        "properties": {
-            "name": {
-                "type": "keyword"
-            },
-            "updated_on": {
-                "type": "date"
-            },
-            "keys_updated_on": {
-                "type": "date"
-            },
-            "auth": {
-                "dynamic": false,
-                "properties": {
-                    "active": {
-                        "dynamic": true,
-                        "type": "object"
-                    },
-                    "owner": {
-                        "dynamic": true,
-                        "type": "object"
+        "_doc": {
+            "properties": {
+                "name": {
+                    "type": "keyword"
+                },
+                "updated_on": {
+                    "type": "date"
+                },
+                "keys_updated_on": {
+                    "type": "date"
+                },
+                "auth": {
+                    "dynamic": false,
+                    "properties": {
+                        "active": {
+                            "dynamic": true,
+                            "type": "object"
+                        },
+                        "owner": {
+                            "dynamic": true,
+                            "type": "object"
+                        }
                     }
                 }
             }
@@ -192,27 +198,29 @@ const block = {
         "index.codec": "best_compression"
     },
     "mappings": {
-        "properties": {
-            "block_num": {
-                "type": "long"
-            },
-            "producer": {
-                "type": "keyword"
-            },
-            "new_producers.producers.block_signing_key": {
-                "enabled": false
-            },
-            "new_producers.producers.producer_name": {
-                "type": "keyword"
-            },
-            "new_producers.version": {
-                "type": "long"
-            },
-            "timestamp": {
-                "type": "date"
-            },
-            "schedule_version": {
-                "type": "double"
+        "_doc": {
+            "properties": {
+                "block_num": {
+                    "type": "long"
+                },
+                "producer": {
+                    "type": "keyword"
+                },
+                "new_producers.producers.block_signing_key": {
+                    "enabled": false
+                },
+                "new_producers.producers.producer_name": {
+                    "type": "keyword"
+                },
+                "new_producers.version": {
+                    "type": "long"
+                },
+                "timestamp": {
+                    "type": "date"
+                },
+                "schedule_version": {
+                    "type": "double"
+                }
             }
         }
     }
