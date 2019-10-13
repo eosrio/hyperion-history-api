e0ee73ac5ce22944523e8f40b3222d672879ebb6
Igor Lins e Silva <igor.linsesilva@gmail.com>
2019-10-12 22:05:34 -0300

    Create test-socket.js

---
+++ test-socket.js
@@ -0,0 +1,89 @@
+const {TextDecoder, TextEncoder} = require('util');
+const {Serialize} = require('eosjs');
+const commander = require('commander');
+const fetch = require('node-fetch');
+const zlib = require('zlib');
+const WebSocket = require('ws');
+
+class Connection {
+    // Connect to the State-History Plugin
+    constructor({socketAddress}) {
+        // Protocol ABI
+        this.abi = null;
+
+        // Types in the protocol ABI
+        this.types = null;
+
+        // Tables in the protocol ABI
+        this.tables = new Map;
+
+        // The socket
+        this.ws = new WebSocket(socketAddress, {perMessageDeflate: false});
+        this.ws.on('message', data => this.onMessage(data));
+    }
+
+    // Convert JSON to binary. type identifies one of the types in this.types.
+    serialize(type, value) {
+        const buffer = new Serialize.SerialBuffer({textEncoder: new TextEncoder, textDecoder: new TextDecoder});
+        Serialize.getType(this.types, type).serialize(buffer, value);
+        return buffer.asUint8Array();
+    }
+
+    // Convert binary to JSON. type identifies one of the types in this.types.
+    deserialize(type, array) {
+        const buffer = new Serialize.SerialBuffer({textEncoder: new TextEncoder, textDecoder: new TextDecoder, array});
+        return Serialize.getType(this.types, type).deserialize(buffer, new Serialize.SerializerState({bytesAsUint8Array: true}));
+    }
+
+    // Serialize a request and send it to the plugin
+    send(request) {
+        this.ws.send(this.serialize('request', request));
+    }
+
+    // Receive a message
+    onMessage(data) {
+        try {
+            if (!this.abi) {
+                // First message is the protocol ABI
+                this.abi = JSON.parse(data);
+                this.types = Serialize.getTypesFromAbi(Serialize.createInitialTypes(), this.abi);
+                for (const table of this.abi.tables)
+                    this.tables.set(table.name, table.type);
+
+                // Send the first request
+                this.send(['get_blocks_request_v0', {
+                    max_messages_in_flight: 10,
+                    have_positions: [],
+                    irreversible_only: false,
+                    fetch_block: true,
+                    fetch_traces: false,
+                    fetch_deltas: false,
+                    start_block_num: 2,
+                    end_block_num: 3
+                }]);
+            } else {
+                // Deserialize and dispatch a message
+                console.log('Data size:', data.length);
+                const [type, response] = this.deserialize('result', data);
+                this[type](response);
+            }
+        } catch (e) {
+            console.log(e);
+            process.exit(1);
+        }
+    }
+
+    // Report status
+    get_blocks_result_v0(response) {
+        console.log('get_blocks_result_v0:');
+        console.log(JSON.stringify(response, null, 4));
+        process.exit(0);
+    }
+} // Connection
+
+commander
+    .option('-a, --socket-address [addr]', 'Socket address', 'ws://localhost:8080/')
+    .option('-f, --first-block [num]', 'Socket address', 2)
+    .parse(process.argv);
+
+const connection = new Connection(commander);
