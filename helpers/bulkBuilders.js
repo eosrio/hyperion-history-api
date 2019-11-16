c939731f874d83c8c57080beb94a6bacce33ea53
Igor Lins e Silva <igor@eosrio.io>
2019-11-16 02:50:53 -0300

    Update bulkBuilders.js

--- helpers/bulkBuilders.js
+++ helpers/bulkBuilders.js
@@ -8,68 +8,65 @@ function makeScriptedOp(id, body) {
     ];
 }
 
-function makeHashedId(id_string) {
-    const hash = crypto.createHash('sha256');
-    return hash.update(id_string).digest('hex');
-}
-
 function buildActionBulk(payloads, messageMap) {
     return _(payloads).map(payload => {
-        const body = JSON.parse(Buffer.from(payload.content).toString());
-        messageMap.set(body['global_sequence'], payload);
-        return [{index: {_id: body['global_sequence']}}, body];
+        const body = JSON.parse(payload.content);
+        const id = body['global_sequence'];
+        messageMap.set(id, _.omit(payload, ['content']));
+        return [{index: {_id: id}}, body];
     }).flatten()['value']();
 }
 
 function buildBlockBulk(payloads, messageMap) {
     return _(payloads).map(payload => {
-        const body = JSON.parse(Buffer.from(payload.content).toString());
-        messageMap.set(body['block_num'], payload);
-        return [{index: {_id: body['block_num']}}, body];
+        const body = JSON.parse(payload.content);
+        const id = body['block_num'];
+        messageMap.set(id, _.omit(payload, ['content']));
+        return [{index: {_id: id}}, body];
     }).flatten()['value']();
 }
 
 function buildAbiBulk(payloads, messageMap) {
     return _(payloads).map(payload => {
-        const body = JSON.parse(Buffer.from(payload.content).toString());
+        const body = JSON.parse(payload.content);
         const id = body['block'] + body['account'];
-        messageMap.set(id, payload);
+        messageMap.set(id, _.omit(payload, ['content']));
         return [{index: {_id: id}}, body];
     }).flatten()['value']();
 }
 
 function buildDeltaBulk(payloads, messageMap) {
     return _(payloads).map(payload => {
-        const body = JSON.parse(Buffer.from(payload.content).toString());
-        const id = makeHashedId(`${body.block_num}-${body.code}-${body.scope}-${body.table}-${body.payer}`);
-        messageMap.set(id, payload);
+        const body = JSON.parse(payload.content);
+        const id = `${body.block_num}-${body.code}-${body.scope}-${body.table}-${body.payer}`;
+        messageMap.set(id, _.omit(payload, ['content']));
         return [{index: {_id: id}}, body];
     }).flatten()['value']();
 }
 
 function buildTableProposalsBulk(payloads, messageMap) {
     return _(payloads)['map'](payload => {
-        const body = JSON.parse(Buffer.from(payload.content).toString());
-        const id = makeHashedId(`${body.proposer}-${body.proposal_name}-${body.primary_key}`);
-        messageMap.set(id, payload);
+        const body = JSON.parse(payload.content);
+        const id = `${body.proposer}-${body.proposal_name}-${body.primary_key}`;
+        messageMap.set(id, _.omit(payload, ['content']));
         return makeScriptedOp(id, body);
     }).flatten()['value']();
 }
 
 function buildTableAccountsBulk(payloads, messageMap) {
     return _(payloads)['map'](payload => {
-        const body = JSON.parse(Buffer.from(payload.content).toString());
-        const id = makeHashedId(`${body.code}-${body.scope}-${body.primary_key}`);
-        messageMap.set(id, payload);
+        const body = JSON.parse(payload.content);
+        const id = `${body.code}-${body.scope}-${body.primary_key}`;
+        messageMap.set(id, _.omit(payload, ['content']));
         return makeScriptedOp(id, body);
     }).flatten()['value']();
 }
 
 function buildTableVotersBulk(payloads, messageMap) {
     return _(payloads)['map'](payload => {
-        const body = JSON.parse(Buffer.from(payload.content).toString());
-        const id = makeHashedId(`${body.primary_key}`);
-        messageMap.set(id, payload);
+        const body = JSON.parse(payload.content);
+        const id = `${body.primary_key}`;
+        messageMap.set(id, _.omit(payload, ['content']));
         return makeScriptedOp(id, body);
     }).flatten()['value']();
 }
