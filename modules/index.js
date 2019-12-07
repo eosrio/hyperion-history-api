4ad3e1eb2627a42c1354e0082cecb6210b672d43
Igor Lins e Silva <igor@eosrio.io>
2019-12-07 11:43:04 -0300

    Update index.js

--- modules/index.js
+++ modules/index.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const path = require('path');
+const chainID = require('../connections.json').chains[process.env.CHAIN]['chain_id'];
 
 class HyperionModuleLoader {
 
     #handledActions = new Map();
     actionParser;
+    chainMappings = new Map();
 
     constructor() {
         this.loadActionHandlers();
@@ -26,18 +28,35 @@ class HyperionModuleLoader {
         }
     }
 
+    includeModule(_module) {
+        if (this.#handledActions.has(_module.contract)) {
+            const existing = this.#handledActions.get(_module.contract);
+            existing.set(_module.action, _module.handler);
+        } else {
+            const _map = new Map();
+            _map.set(_module.action, _module.handler);
+            this.#handledActions.set(_module.contract, _map);
+        }
+    }
+
     loadActionHandlers() {
         const files = fs.readdirSync('modules/action_data/');
         for (const plugin of files) {
             const _module = require(path.join(__dirname, 'action_data', plugin)).hyperionModule;
             if (_module.parser_version.includes(process.env.PARSER)) {
-                if (this.#handledActions.has(_module.contract)) {
-                    const existing = this.#handledActions.get(_module.contract);
-                    existing.set(_module.action, _module.handler);
-                } else {
-                    const _map = new Map();
-                    _map.set(_module.action, _module.handler);
-                    this.#handledActions.set(_module.contract, _map);
+                if (_module.chain === chainID || _module.chain === '*') {
+                    const key = `${_module.contract}::${_module.action}`;
+                    if (this.chainMappings.has(key)) {
+                        if (this.chainMappings.get(key) === '*' && _module.chain === chainID) {
+                            // console.log('Overwriting module ' + key + ' for ' + _module.chain);
+                            this.includeModule(_module);
+                            this.chainMappings.set(key, _module.chain);
+                        }
+                    } else {
+                        // console.log('Including module ' + key + ' for ' + _module.chain);
+                        this.includeModule(_module);
+                        this.chainMappings.set(key, _module.chain);
+                    }
                 }
             }
         }
