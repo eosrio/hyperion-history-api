b53464c82440afa4a002a6b20c0d8ccfd200a6e1
Igor Lins e Silva <igor@eosrio.io>
2020-04-09 23:36:31 -0300

    FIO custom actions

---
+++ modules/action_data/fio-fdtnrwdupdat.js
@@ -0,0 +1,25 @@
+const hyperionModule = {
+  chain: '21dcae42c0182200e93f954a074011f9048a7624c6fe81d3c9541a614a88bd1c',
+  contract: 'fio.treasury',
+  action: 'fdtnrwdupdat',
+  parser_version: ['1.8'],
+  mappings: {
+    action: {
+      '@fdtnrwdupdat': {
+        'properties': {
+          'amount': {'type': 'long'},
+        },
+      },
+    },
+  },
+  handler: (action) => {
+    // attach action extras here
+    const data = action['act']['data'];
+    action['@fdtnrwdupdat'] = {
+      amount: data['amount'],
+    };
+    delete action['act']['data'];
+  },
+};
+
+module.exports = {hyperionModule};
