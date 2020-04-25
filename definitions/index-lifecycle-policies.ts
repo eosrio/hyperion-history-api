39513f1716738bafa459db85374bfc8e54e1f25b
Igor Lins e Silva <igor@eosrio.io>
2020-04-25 07:50:16 -0300

    new ILP

--- definitions/index-lifecycle-policies.ts
+++ definitions/index-lifecycle-policies.ts
@@ -1,23 +1,47 @@
-export const ILPs = [
+import {IlmPutLifecycle} from "@elastic/elasticsearch/api/requestParams";
+
+export const ILPs: IlmPutLifecycle[] = [
     {
-        "policy": "50G30D",
-        "body": {
-            "policy": {
-                "phases": {
-                    "hot": {
-                        "min_age": "0ms",
-                        "actions": {
-                            "rollover": {
-                                "max_age": "30d",
-                                "max_size": "50gb"
+        policy: "50G30D",
+        body: {
+            policy: {
+                phases: {
+                    hot: {
+                        min_age: "0ms",
+                        actions: {
+                            rollover: {
+                                max_age: "30d",
+                                max_size: "50gb"
                             },
-                            "set_priority": {
-                                "priority": 100
+                            set_priority: {
+                                priority: 100
                             }
                         }
                     }
                 }
             }
         }
-    }
+    },
+    {
+        policy: "10G30D",
+        body: {
+            policy: {
+                phases: {
+                    hot: {
+                        min_age: "0ms",
+                        actions: {
+                            rollover: {
+                                max_age: "30d",
+                                max_size: "10gb",
+                                max_docs: 100000000
+                            },
+                            set_priority: {
+                                priority: 100
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    },
 ];
