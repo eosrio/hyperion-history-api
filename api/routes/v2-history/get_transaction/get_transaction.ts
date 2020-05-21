cb2b8bfdef1a838cc033fd9f7f1f50d76de2db43
Igor Lins e Silva <igor@eosrio.io>
2020-05-21 06:30:50 -0300

    [api] add generated trx to get_transaction

--- api/routes/v2-history/get_transaction/get_transaction.ts
+++ api/routes/v2-history/get_transaction/get_transaction.ts
@@ -3,39 +3,72 @@ import {ServerResponse} from "http";
 import {mergeActionMeta, timedQuery} from "../../../helpers/functions";
 
 async function getTransaction(fastify: FastifyInstance, request: FastifyRequest) {
+    const _size = fastify.manager.config.api.limits.get_trx_actions || 100;
     const pResults = await Promise.all([
         fastify.eosjs.rpc.get_info(),
         fastify.elastic.search({
-            "index": fastify.manager.chain + '-action-*',
-            "size": fastify.manager.config.api.limits.get_trx_actions || 100,
-            "body": {
-                "query": {
-                    "bool": {
+            index: fastify.manager.chain + '-action-*',
+            size: _size,
+            body: {
+                query: {
+                    bool: {
                         must: [
-                            {term: {"trx_id": request.query.id.toLowerCase()}}
+                            {term: {trx_id: request.query.id.toLowerCase()}}
                         ]
                     }
                 },
-                "sort": {
-                    "global_sequence": "asc"
+                sort: {
+                    global_sequence: "asc"
+                }
+            }
+        }),
+        fastify.elastic.search({
+            index: fastify.manager.chain + '-gentrx-*',
+            size: _size,
+            body: {
+                query: {
+                    bool: {
+                        must: [
+                            {term: {trx_id: request.query.id.toLowerCase()}}
+                        ]
+                    }
                 }
             }
         })
     ]);
+
     const results = pResults[1];
+    const genTrxRes = pResults[2];
+
     const response = {
+        "executed": false,
         "trx_id": request.query.id,
         "lib": pResults[0].last_irreversible_block_num,
-        "actions": []
+        "actions": [],
+        "generated": undefined
     };
+
     const hits = results['body']['hits']['hits'];
+
     if (hits.length > 0) {
         for (let action of hits) {
             action = action._source;
             mergeActionMeta(action);
             response.actions.push(action);
         }
+        response.executed = true;
     }
+
+    const hits2 = genTrxRes['body']['hits']['hits'];
+
+    if (hits2 && hits2.length > 0) {
+        if (hits2[0]._source['@timestamp']) {
+            hits2[0]._source['timestamp'] = hits2[0]._source['@timestamp'];
+            delete hits2[0]._source['@timestamp'];
+        }
+        response.generated = hits2[0]._source;
+    }
+
     return response;
 }
 
