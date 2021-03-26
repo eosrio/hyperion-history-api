417bb96294673d9bd2c16f125f36eb75fd1fab38
Igor Lins e Silva <igor.linsesilva@gmail.com>
2021-03-26 02:58:30 -0300

    [indexer] fix amqp credentials encoding

--- connections/amqp.ts
+++ connections/amqp.ts
@@ -1,75 +1,85 @@
 import {debugLog, hLog} from "../helpers/common_functions";
-import got from "got";
+import got, {HTTPError} from "got";
 import {connect, Connection} from 'amqplib';
 
 export async function createConnection(config): Promise<Connection> {
-    try {
-        const amqp_url = getAmpqUrl(config);
-        const conn: Connection = await connect(amqp_url);
-        debugLog("[AMQP] connection established");
-        return conn;
-    } catch (e) {
-        console.log("[AMQP] failed to connect!");
-        console.log(e.message);
-        await new Promise(resolve => setTimeout(resolve, 5000));
-        return await createConnection(config);
-    }
+	try {
+		const amqp_url = getAmpqUrl(config);
+		const conn: Connection = await connect(amqp_url);
+		debugLog("[AMQP] connection established");
+		return conn;
+	} catch (e) {
+		console.log("[AMQP] failed to connect!");
+		console.log(e.message);
+		await new Promise(resolve => setTimeout(resolve, 5000));
+		return await createConnection(config);
+	}
 }
 
 export function getAmpqUrl(config): string {
-    return `amqp://${config.user}:${config.pass}@${config.host}/%2F${config.vhost}`;
+	const u = encodeURIComponent(config.user);
+	const p = encodeURIComponent(config.pass);
+	const v = encodeURIComponent(config.vhost)
+	return `amqp://${u}:${p}@${config.host}/${v}`;
 }
 
 
 async function createChannels(connection) {
-    try {
-        const channel = await connection.createChannel();
-        const confirmChannel = await connection.createConfirmChannel();
-        return [channel, confirmChannel];
-    } catch (e) {
-        console.log("[AMQP] failed to create channels");
-        console.error(e);
-        return null;
-    }
+	try {
+		const channel = await connection.createChannel();
+		const confirmChannel = await connection.createConfirmChannel();
+		return [channel, confirmChannel];
+	} catch (e) {
+		console.log("[AMQP] failed to create channels");
+		console.error(e);
+		return null;
+	}
 }
 
 export async function amqpConnect(onReconnect, config, onClose) {
-    let connection = await createConnection(config);
-    if (connection) {
-        const channels = await createChannels(connection);
-        if (channels) {
-            connection.on('error', (err) => {
-                hLog(err.message);
-            });
-            connection.on('close', () => {
-                hLog('Connection closed!');
-                onClose();
-                setTimeout(async () => {
-                    hLog('Retrying in 5 seconds...');
-                    const _channels = await amqpConnect(onReconnect, config, onClose);
-                    onReconnect(_channels);
-                    return _channels;
-                }, 5000);
-            });
-            return channels;
-        } else {
-            return null;
-        }
-    } else {
-        return null;
-    }
+	let connection = await createConnection(config);
+	if (connection) {
+		const channels = await createChannels(connection);
+		if (channels) {
+			connection.on('error', (err) => {
+				hLog(err.message);
+			});
+			connection.on('close', () => {
+				hLog('Connection closed!');
+				onClose();
+				setTimeout(async () => {
+					hLog('Retrying in 5 seconds...');
+					const _channels = await amqpConnect(onReconnect, config, onClose);
+					onReconnect(_channels);
+					return _channels;
+				}, 5000);
+			});
+			return channels;
+		} else {
+			return null;
+		}
+	} else {
+		return null;
+	}
 }
 
 export async function checkQueueSize(q_name, config) {
-    try {
-        const apiUrl = `http://${config.api}/api/queues/%2F${config.vhost}/${q_name}`;
-        const result = JSON.parse((await got(apiUrl, {
-            username: config.user,
-            password: config.pass
-        })).body);
-        return result.messages;
-    } catch (e) {
-        hLog('[WARNING] Checking queue size failed, HTTP API is not ready!');
-        return 0;
-    }
+	try {
+		const v = encodeURIComponent(config.vhost);
+		const apiUrl = `http://${config.api}/api/queues/${v}/${encodeURIComponent(q_name)}`;
+		const opts = {
+			username: config.user,
+			password: config.pass
+		};
+		const data = await got.get(apiUrl, opts).json() as any;
+		return data.messages;
+	} catch (e) {
+		hLog('[WARNING] Checking queue size failed, HTTP API is not ready!');
+		if (e instanceof HTTPError) {
+			hLog(e.response);
+		} else {
+			hLog(JSON.stringify(e, null, 2));
+		}
+		return 0;
+	}
 }
