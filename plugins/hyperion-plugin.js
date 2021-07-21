630f0e286c6eed51571cd1e4d17e5b8c36d48ed9
Igor Lins e Silva <igor.linsesilva@gmail.com>
2021-07-21 07:52:18 -0300

    update packages

---
+++ plugins/hyperion-plugin.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HyperionPlugin = void 0;
+class HyperionPlugin {
+    constructor() {
+        this.dynamicContracts = [];
+    }
+}
+exports.HyperionPlugin = HyperionPlugin;
+//# sourceMappingURL=hyperion-plugin.js.map
