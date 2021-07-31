99c43af7bffd3c563737fcb641143b096d93dbf0
Igor Lins e Silva <igor.linsesilva@gmail.com>
2021-07-31 04:45:52 -0300

    docker updates

--- run.sh
+++ run.sh
@@ -4,8 +4,17 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 echo -e "\n-->> Starting $1..."
-(set -x; pm2 start --only "$@" --update-env --silent)
+(
+  set -x
+  pm2 start --only "$@" --update-env --silent
+)
 echo -e "\n-->> Saving pm2 state..."
-(set -x; pm2 save)
+(
+  set -x
+  pm2 save
+)
 echo -e "\n-->> Reading $1 logs..."
-(set -x; pm2 logs --raw --lines 10 "$@")
+(
+  set -x
+  pm2 logs --raw --lines 10 "$@"
+)
