1bb1d530aa51198b4533b9f5d3889c7b28322f77
BasharKhouri-Voice <bkhoury@voice.com>
2022-03-04 23:52:20 +0300

    vault support

--- scripts/get-secrets.sh
+++ scripts/get-secrets.sh
@@ -1,3 +1,8 @@
-jq --arg elastic_user $elastic_user --arg  elastic_pass $elastic_pass '.elasticsearch.user = $elastic_user | .elasticsearch.pass =  $elastic_pass ' /tmp/connections.json > /hyperion-history-api/connections.json
+jq --arg elastic_user $elastic_user --arg  elastic_pass $elastic_pass --arg elastic_host $elastic_host
+    '.elasticsearch.user = $elastic_user | .elasticsearch.pass =  $elastic_pass | .elasticsearch.host = [$elastic_host] |
+    .chains.voice.http = $chain_http ' /tmp/connections.json > /hyperion-history-api/connections.json
+
+jq --arg  server_name $server_name \
+    '.api.server_name = $server_name'  /temp/voice.config.json> chains/example.config.json
 
 ./run.sh $@
