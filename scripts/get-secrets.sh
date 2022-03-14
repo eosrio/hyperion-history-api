ccf8cab891d484e65dd794512f69339df6262ed0
Bashar Khouri <92811050+BasharKhouri-Voice@users.noreply.github.com>
2022-03-15 01:05:38 +0300

    Reset hyperion (#32)
    * reset_hyperion
    
    * fix error in the get-secrets script
    
    * fix spaceing

--- scripts/get-secrets.sh
+++ scripts/get-secrets.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eu
-jq -n --arg  elastic_user $ELASTIC_USER  --arg  elastic_pass $ELASTIC_PASS --arg elastic_host $ELASTIC_HOST \
-    '.elasticsearch.user = $elastic_user | .elasticsearch.pass =  $elastic_pass | .elasticsearch.host = $elastic_host | .elasticsearch.ingest_nodes = [$elastic_host] '  \
+jq --arg elastic_user $ELASTIC_USER --arg elastic_pass $ELASTIC_PASS --arg elastic_host $ELASTIC_HOST \
+    '.elasticsearch.user = $elastic_user | .elasticsearch.pass = $elastic_pass | .elasticsearch.host = $elastic_host | .elasticsearch.ingest_nodes = [$elastic_host]' \
     /tmp/connections.json > /hyperion-history-api/connections.json
 
-jq -n --arg server_name $SERVER_NAME \
-    '.api.server_name = $server_name'  /tmp/voice.config.json > /hyperion-history-api/chains/voice.config.json
+jq  --arg server_name $SERVER_NAME \
+    '.api.server_name = $server_name' /tmp/voice.config.json > /hyperion-history-api/chains/voice.config.json
 ./run.sh $@
