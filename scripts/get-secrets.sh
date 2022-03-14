d9d4fb9626f6a8e64296a225743dcd80b0b00a52
Bashar Khouri <92811050+BasharKhouri-Voice@users.noreply.github.com>
2022-03-14 22:39:13 +0300

    Test proxy (#28)
    * fix config file location
    
    * test proxy server adder localhost
    
    * fix extra vars
    
    * remove proxy from rabbit mq
    
    * test pod_ip
    
    * test pod_ip
    
    * fix script
    
    * added env vars
    
    * change the connectons.json location
    
    * fixing empty str
    
    * added POD_IP to the jsonfile
    
    * added quates to the pod_ip env var
    
    * added quates to the pod_ip env var
    
    * added extra vars
    
    * change the no_proxy value
    
    * added 0.0.0.0 to the no_proxy
    
    * pring proxy from vault
    
    * remove vault proxy var
    
    * fix the run script
    
    * fix prod.yaml and prod buildkit
    
    * fix the get_secret script
    
    * added vault for prod
    
    * update the noproxy and the prod vault path
    
    * fix vault location for prod

--- scripts/get-secrets.sh
+++ scripts/get-secrets.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
 set -eu
-
-jq --arg elastic_user $ELASTIC_USER --arg  elastic_pass $ELASTIC_PASS --arg elastic_host $ELASTIC_HOST \
+jq -n --arg  elastic_user $ELASTIC_USER  --arg  elastic_pass $ELASTIC_PASS --arg elastic_host $ELASTIC_HOST \
     '.elasticsearch.user = $elastic_user | .elasticsearch.pass =  $elastic_pass | .elasticsearch.host = $elastic_host | .elasticsearch.ingest_nodes = [$elastic_host] '  \
     /tmp/connections.json > /hyperion-history-api/connections.json
 
-jq --arg  server_name $SERVER_NAME \
-    '.api.server_name = $server_name'  /tmp/voice.config.json> /hyperion-history-api/chains/voice.config.json
-
+jq -n --arg server_name $SERVER_NAME \
+    '.api.server_name = $server_name'  /tmp/voice.config.json > /hyperion-history-api/chains/voice.config.json
 ./run.sh $@
