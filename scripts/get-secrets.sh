444694bcffbe42e5f4eb9e3f6d9e51580e674e16
Bashar Khouri <92811050+BasharKhouri-Voice@users.noreply.github.com>
2022-05-11 19:13:02 +0300

    fix output location (#60)

--- scripts/get-secrets.sh
+++ scripts/get-secrets.sh
@@ -2,8 +2,8 @@
 set -eu
 jq --arg elastic_user $ELASTIC_USER --arg elastic_pass $ELASTIC_PASS --arg elastic_host $ELASTIC_HOST \
     '.elasticsearch.user = $elastic_user | .elasticsearch.pass = $elastic_pass | .elasticsearch.host = $elastic_host | .elasticsearch.ingest_nodes = [$elastic_host]' \
-    /tmp/connections.json > /hyperion-history-api/connections.json
+    /tmp/connections.json > /opt/app/connections.json
 
 jq  --arg server_name $SERVER_NAME \
-    '.api.server_name = $server_name' /tmp/voice.config.json > /hyperion-history-api/chains/voice.config.json
+    '.api.server_name = $server_name' /tmp/voice.config.json > /opt/app/chains/voice.config.json
 ./run.sh $@
