821beda6f3b4d6075f5c19d99e1a017bef00fc91
Bashar Khouri <92811050+BasharKhouri-Voice@users.noreply.github.com>
2022-05-26 19:03:43 +0300

    VCO-1407-add-LD-secrets (#68)
    * VCO-1407-add-LD-secrets
    
    * turn off debug

--- scripts/get-secrets.sh
+++ scripts/get-secrets.sh
@@ -4,6 +4,6 @@ jq --arg elastic_user $ELASTIC_USER --arg elastic_pass $ELASTIC_PASS --arg elast
     '.elasticsearch.user = $elastic_user | .elasticsearch.pass = $elastic_pass | .elasticsearch.host = $elastic_host | .elasticsearch.ingest_nodes = [$elastic_host]' \
     /tmp/connections.json > /opt/app/connections.json
 
-jq  --arg server_name $SERVER_NAME \
-    '.api.server_name = $server_name' /tmp/voice.config.json > /opt/app/chains/voice.config.json
+jq  --arg server_name $SERVER_NAME --arg LAUNCHDARKLY_SDK_KEY $LAUNCHDARKLY_SDK_KEY --arg LAUNCHDARKLY_CLIENT_SIDE_ID $LAUNCHDARKLY_CLIENT_SIDE_ID \
+    '.api.server_name = $server_name |.launchdarkly.sdk_key = $LAUNCHDARKLY_CLIENT_SIDE_ID | .launchdarkly.clinet_side_id = $LAUNCHDARKLY_CLIENT_SIDE_ID' /tmp/voice.config.json > /opt/app/chains/voice.config.json
 ./run.sh $@
