fbf0f634c939e9a8728c0eb19504fb2d5a0ec867
Igor Lins e Silva <igor.linsesilva@gmail.com>
2022-09-11 05:40:44 -0300

    add more information and interfaces

--- api/routes/v2/health/health.ts
+++ api/routes/v2/health/health.ts
@@ -3,7 +3,9 @@ import {connect} from "amqplib";
 import {timedQuery} from "../../../helpers/functions";
 import {getFirstIndexedBlock, getLastIndexedBlockWithTotalBlocks} from "../../../../helpers/common_functions";
 
-async function checkRabbit(fastify: FastifyInstance) {
+
+
+async function checkRabbit(fastify: FastifyInstance): Promise<ServiceResponse<any>> {
     try {
         const connection = await connect(fastify.manager.ampqUrl);
         await connection.close();
@@ -14,7 +16,15 @@ async function checkRabbit(fastify: FastifyInstance) {
     }
 }
 
-async function checkNodeos(fastify: FastifyInstance) {
+interface NodeosService {
+    head_block_num: number;
+    head_block_time: string;
+    time_offset: number;
+    last_irreversible_block: number;
+    chain_id: string;
+}
+
+async function checkNodeos(fastify: FastifyInstance): Promise<ServiceResponse<NodeosService>> {
     const rpc = fastify.manager.nodeosJsonRPC;
     try {
         const results = await rpc.get_info();
@@ -35,24 +45,45 @@ async function checkNodeos(fastify: FastifyInstance) {
     }
 }
 
-async function checkElastic(fastify: FastifyInstance) {
+interface ESService {
+    first_indexed_block: number;
+    last_indexed_block: number;
+    total_indexed_blocks: number;
+    active_shards: string;
+    missing_blocks: number;
+    missing_pct: string;
+    head_offset: number;
+}
+
+interface ServiceResponse<T> {
+    service: string;
+    time: number;
+    status: any
+    service_data?: T;
+}
+
+async function checkElastic(fastify: FastifyInstance): Promise<ServiceResponse<ESService>> {
     try {
         let esStatus = await fastify.elastic.cat.health({format: 'json', v: true});
-        let firstIndexedBlock: number = -1;
+        let firstIndexedBlock: number;
         const fib = await fastify.redis.get(`${fastify.manager.chain}::fib`);
         if (fib) {
             firstIndexedBlock = parseInt(fib);
         } else {
             firstIndexedBlock = await getFirstIndexedBlock(fastify.elastic, fastify.manager.chain);
-            await fastify.redis.set(`${fastify.manager.chain}::fib`,firstIndexedBlock);
+            await fastify.redis.set(`${fastify.manager.chain}::fib`, firstIndexedBlock);
         }
         let indexedBlocks = await getLastIndexedBlockWithTotalBlocks(fastify.elastic, fastify.manager.chain);
-        const data = {
+        const missingCounter = (indexedBlocks[0] - firstIndexedBlock) - indexedBlocks[1];
+        const missingPct = (missingCounter * 100 / indexedBlocks[1]).toFixed(2) + "%";
+        const data: ESService = {
             first_indexed_block: firstIndexedBlock,
             last_indexed_block: indexedBlocks[0],
             total_indexed_blocks: indexedBlocks[1] + 1,
             active_shards: esStatus.body[0]['active_shards_percent'],
-            missing_blocks: (indexedBlocks[0] - firstIndexedBlock) - indexedBlocks[1]
+            missing_blocks: (indexedBlocks[0] - firstIndexedBlock) - indexedBlocks[1],
+            missing_pct: missingPct,
+            head_offset: null
         };
         let stat = 'OK';
         esStatus.body.forEach(status => {
@@ -69,7 +100,7 @@ async function checkElastic(fastify: FastifyInstance) {
     }
 }
 
-function createHealth(name: string, status, data?: any) {
+function createHealth(name: string, status, data?: any): ServiceResponse<any> {
     let time = Date.now();
     return {
         service: name,
@@ -79,7 +110,7 @@ function createHealth(name: string, status, data?: any) {
     }
 }
 
-async function getHealthQuery(fastify: FastifyInstance, request: FastifyRequest) {
+async function getHealthQuery(fastify: FastifyInstance) {
     let response = {
         version: fastify.manager.current_version,
         version_hash: fastify.manager.getServerHash(),
@@ -87,9 +118,14 @@ async function getHealthQuery(fastify: FastifyInstance, request: FastifyRequest)
         health: [],
         features: fastify.manager.config.features
     };
-    response.health.push(await checkRabbit(fastify));
-    response.health.push(await checkNodeos(fastify));
-    response.health.push(await checkElastic(fastify));
+    response.health = await Promise.all([
+        checkRabbit(fastify),
+        checkNodeos(fastify),
+        checkElastic(fastify)
+    ]);
+    const es = response.health.find(value => value.service === 'Elasticsearch');
+    const nodeos = response.health.find(value => value.service === 'NodeosRPC');
+    es.service_data.v = nodeos.service_data.head_block_num - es.service_data.last_indexed_block;
     return response;
 }
 
