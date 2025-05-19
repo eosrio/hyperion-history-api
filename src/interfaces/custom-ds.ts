def93cab34c1eee5da04a176059a367166a113a2
Igor Lins e Silva <4753812+igorls@users.noreply.github.com>
2025-05-19 10:12:34 +0000

    define TokenAccount using decorators

--- src/interfaces/custom-ds.ts
+++ src/interfaces/custom-ds.ts
@@ -1,12 +1,15 @@
 import {Asset, Struct} from "@wharfkit/antelope";
 
+@Struct.type("accounts")
 export class TokenAccount extends Struct {
-    static abiName = "accounts";
-    static abiFields = [
-        {
-            name: "balance",
-            type: Asset
-        }
-    ];
-    balance!: Asset
+    @Struct.field(Asset) balance!: Asset
 }
+
+// Alternative way to define the class using static properties
+// export class TokenAccount extends Struct {
+//     static abiName = "accounts";
+//     static abiFields = [
+//         {name: "balance", type: Asset}
+//     ];
+//     balance!: Asset
+// }
