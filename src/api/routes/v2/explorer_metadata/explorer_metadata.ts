6ef8c5cb35cc0fc9f43f4c21fe51fff41759c609
Igor Lins e Silva <4753812+igorls@users.noreply.github.com>
2025-06-18 23:08:49 +0000

    feat: Update getExplorerMetadata to use hasRoute for oracle check and format imports

--- src/api/routes/v2/explorer_metadata/explorer_metadata.ts
+++ src/api/routes/v2/explorer_metadata/explorer_metadata.ts
@@ -1,7 +1,10 @@
-import {FastifyInstance, FastifyReply, FastifyRequest} from "fastify";
-import {timedQuery} from "../../../helpers/functions.js";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { timedQuery } from "../../../helpers/functions.js";
 
 async function getExplorerMetadata(fastify: FastifyInstance) {
+
+
+
     return {
         provider: fastify.manager.config.api.provider_name,
         provider_url: fastify.manager.config.api.provider_url,
@@ -9,7 +12,7 @@ async function getExplorerMetadata(fastify: FastifyInstance) {
         chain_id: fastify.manager.conn.chains[fastify.manager.chain].chain_id,
         custom_core_token: fastify.manager.config.api.custom_core_token,
         theme: fastify.explorerTheme ?? {},
-        oracle: fastify.manager.config.api.explorer?.oracle ?? null,
+        oracle: fastify.hasRoute({ url: '/v2/oracle/get_datapoints_histogram', method: 'GET' })
     }
 }
 
